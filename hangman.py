15a627420cb8d83bf1b42f8ce8c20065fb7a63eb
HappyLife2 <130459769+HappyLife2@users.noreply.github.com>
2024-03-21 10:11:30 +0530

    First Commit

---
+++ hangman.py
@@ -0,0 +1,135 @@
+import random
+
+# List of words to guess
+words = ["python", "java", "kotlin", "javascript", "hangman"]
+
+def get_word():
+    return random.choice(words).upper()
+
+def play_hangman():
+    word = get_word()
+    word_completion = "_" * len(word)  # Represent word as blanks
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6  # Number of allowed mistakes
+
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion + "\n")
+
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion + "\n")
+
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+def display_hangman(tries):
+    stages = [  # Final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                  ---
+                """,
+                # Head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                  ---
+                """,
+                # Head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                  ---
+                """,
+                # Head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                  ---
+                """,
+                # Head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                  ---
+                """,
+                # Head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                  ---
+                """,
+                # Initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                  ---
+                """
+    ]
+    return stages[tries]
+
+if __name__ == "__main__":
+    play_hangman()
